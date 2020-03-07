0d90436f97cd40a74c98950a8160ae985e3c2d5c
Christian Gröling <ch.groeling@icloud.com>
2020-03-07 19:24:52 +0100

    Parsing of elements added

--- count_locs.py
+++ count_locs.py
@@ -1,20 +1,64 @@
 import subprocess
 import pprint
 import json
-import dateutil.parser
+import xml.etree.ElementTree as ET
+
+def parse_cloc_result(root):
+
+    header = {}
+
+    for item in root.find('header'):
+        header[item.tag] = item.text
+    
+    header['elapsed_seconds'] = float(header['elapsed_seconds'])
+    header['files_per_second'] = float(header['files_per_second'])
+    header['lines_per_second'] = float(header['lines_per_second'])
+
+    header['n_files'] = int(header['n_files'])
+    header['n_lines'] = int(header['n_lines'])
+    
+    languages = {}
+    for item in root.iter('language'):
+        attribs = item.attrib
+        languages[attribs['name']] = {
+            'files_count' : int(attribs['files_count']),
+            'blank' : int(attribs['blank']),
+            'comment' : int(attribs['comment']),
+            'code' : int(attribs['code'])
+        }
+
+    
+    data={
+        'header' : header,
+        'languages' : languages
+    }
+    return data
+def cloc_on_commit(hash):
+    git_checkout(hash)
+    args=['cloc','-xml','-q','.']
+    print(args)
+    data = subprocess.check_output(args).decode('utf-8').strip()
+    root = ET.fromstring(data)
+    i = parse_cloc_result(root)
+    pprint.pprint(i)
+
+def git_checkout(hash):
+    args=['git','checkout','-q', hash]
+    subprocess.check_output(args).decode('utf-8').split("\n")
 
 def main():
-    args=['git','rev-list','--date=iso8601','--pretty=format:{"hash":"%h","date":"%cd"}', 'HEAD']
-    output = subprocess.check_output(args).decode('utf-8').split("\n")
-    commits = [json.loads(x) for x  in output[1::2]]
-    commits = [
-        {
-            'hash' : x['hash'], 
-            'date': dateutil.parser.parse(x['date'])
-        } 
-        for x in commits
-    ]
+    args=['git','rev-parse', 'HEAD']
+
+    initialCommit = subprocess.check_output(args).decode('utf-8').split("\n")[0]
+
+
+    args=['git','rev-list', 'HEAD']
+    commits = subprocess.check_output(args).decode('utf-8').split("\n")
+    commits = commits[0:len(commits)-1]
+    for i in commits:
+        cloc_on_commit(i)
     
-    pprint.pprint(commits)
+    git_checkout(initialCommit)
+    
 if __name__ == "__main__":
     main()
